f0fce956c719cfa2d3c1675b06a187b6dd0f400d
Jassapb09 <153945294+Jassapb09@users.noreply.github.com>
2025-05-02 01:52:02 +0300

    Program.cs

---
+++ SkinCs/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Guidzgo
+{
+	internal static class Program
+	{
+		/// <summary>
+		/// Hlavní vstupní bod aplikace.
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			try
+			{
+				Application.EnableVisualStyles();
+				Application.SetCompatibleTextRenderingDefault(false);
+				Application.Run(Form1.Instance);
+			}
+			catch (Exception ex)
+			{
+				Clipboard.SetText(ex.ToString());
+				MessageBox.Show(ex.ToString());
+			}
+			
+		}
+	}
+}
